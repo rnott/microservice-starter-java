9bd1660f70e3d66ebda708891acfcc30f9c61827
Randy Nott <1017506+rnott@users.noreply.github.com>
2024-03-11 09:58:31 -0700

    Fix model inheritance

--- prototype/impl/src/main/java/org/rnott/example/ExampleApiImpl.java
+++ prototype/impl/src/main/java/org/rnott/example/ExampleApiImpl.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.rnott.example.api.Example;
 import org.rnott.example.api.ExampleApi;
 import org.rnott.example.api.PageOfExamples;
-import org.rnott.example.api.PatchDocument;
+import org.rnott.example.api.PatchRequest;
 import org.rnott.example.persistence.ExampleEntity;
 import org.rnott.example.persistence.ExampleMapper;
 import org.rnott.example.persistence.ExampleRepository;
@@ -88,7 +88,7 @@ public class ExampleApiImpl extends Application implements ExampleApi {
     }
 
     @Override
-    public Example patch(UUID id, List<PatchDocument> patchDocument) {
+    public Example patch(UUID id, PatchRequest patchRequest) {
         ExampleEntity entity = repository.findById(id)
                 .orElseThrow(() -> new NotFoundException(String.format("id: %s", id)));
         // TODO: implement patch
